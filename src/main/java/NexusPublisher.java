110407045b1275ef51fe784679db323ecea6e803
Jesper Skov <jskov@mada.dk>
2024-03-10 10:54:46 +0100

    final check args

--- src/main/java/NexusPublisher.java
+++ src/main/java/NexusPublisher.java
@@ -1,13 +1,14 @@
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 class NexusPublisher {
     public static void main(String[] args) {
-        System.out.println("Args: " + args);
+        System.out.println("Args: " + Arrays.toString(args));
         System.out.println("Environment:");
-        System.getenv().entrySet().stream()
-            .map(e -> " '" + e.getKey() + "=" + e.getValue() + "'")
-            .sorted()
-            .forEach(System.out::println);
+//        System.getenv().entrySet().stream()
+//            .map(e -> " '" + e.getKey() + "=" + e.getValue() + "'")
+//            .sorted()
+//            .forEach(System.out::println);
         
         System.out.println("CWD: " + Paths.get(".").toAbsolutePath());
         
