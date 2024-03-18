979c468470f0cd6193392f8cb3d35ebdfeb5b0d0
Jesper Skov <jskov@mada.dk>
2024-03-18 07:59:50 +0100

    better warning

--- src/main/java/dk/mada/action/util/LoggerConfig.java
+++ src/main/java/dk/mada/action/util/LoggerConfig.java
@@ -3,6 +3,7 @@ package dk.mada.action.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
@@ -35,8 +36,9 @@ public final class LoggerConfig {
      * @param path resource path of logger configuration
      */
     public static void loadConfig(String path) {
-        try (InputStream configFile = LoggerConfig.class.getResourceAsStream(path)) {
-            LogManager.getLogManager().updateConfiguration(configFile, null);
+        try (InputStream is = LoggerConfig.class.getResourceAsStream(path)) {
+            InputStream validIs = Objects.requireNonNull(is, "Failed to find resource path: " + path);
+            LogManager.getLogManager().updateConfiguration(validIs, null);
         } catch (IOException e) {
             throw new UncheckedIOException("Failed to load logging properties", e);
         }
